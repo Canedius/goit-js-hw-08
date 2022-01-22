f12af1f062c374e15a393cd1849d42a55505a368
Canedius <88346421+Canedius@users.noreply.github.com>
2022-01-22 16:38:14 +0000

    Deploying to gh-pages from @ Canedius/goit-js-hw-08@9462efdb00c3e5dab2677d68eb840c3143969e3d 🚀

--- 01-gallery.d8272bb7.js
+++ 01-gallery.d8272bb7.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=require("./gallery-items"),l=r(require("simplelightbox"));function r(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css");const i=document.querySelector(".gallery"),a=e.galleryItems.map(({preview:e,original:l,description:r})=>`<a class="gallery__item" href="${l}">\n  <img class="gallery__image" src="${e}" alt="${r}"/>\n</a>`);i.insertAdjacentHTML("afterbegin",a.join(""));var t=new l.default(".gallery a",{});console.log(e.galleryItems);
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.c13e2a92.js.map
+//# sourceMappingURL=/parcel-project-template/01-gallery.d8272bb7.js.map
